e5574fc4186616a95ab6d0e357f693f35271ccc5
jeffthedeveloper <jeffersonfir@gmail.com>
2025-03-02 19:09:09 -0300

    Implement basic chatbot with predefined responses

---
+++ chatbot_model.py
@@ -0,0 +1,11 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+model_name = 'gpt2'
+model = GPT2LMHeadModel.from_pretrained(model_name)
+tokenizer = GPT2Tokenizer.from_pretrained(model_name)
+
+def get_gpt_response(prompt):
+    inputs = tokenizer.encode(prompt, return_tensors="pt")
+    outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return response
